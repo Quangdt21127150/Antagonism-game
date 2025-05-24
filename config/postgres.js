5cc2b5a581778c7e8186f07fd693cd5d5cac0f6f
Quang <nghnquang2003@gmail.com>
2025-05-24 13:11:32 +0700

    Update postgres.js

--- config/postgres.js
+++ config/postgres.js
@@ -1,9 +1,11 @@
 const { Sequelize } = require("sequelize");
+const pg = require("pg");
 
 const sequelize = new Sequelize(
   process.env.POSTGRES_SINGAPORE_CONNECTION_STRING,
   {
     dialect: "postgres",
+    dialectModule: pg,
     logging: false,
   }
 );
